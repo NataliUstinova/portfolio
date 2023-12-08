3211b5ed836cecf0c6c32cf525f7930bf3b55a3d
Natali Ustinova <nata@purple.studio>
2023-12-08 22:35:58 +0200

    feat: Google Analytics theme

--- components/theme-switch.tsx
+++ components/theme-switch.tsx
@@ -2,14 +2,22 @@
 import React from "react";
 import { BsMoon, BsSun } from "react-icons/bs";
 import { useTheme } from "@/contexts/theme-context";
-
+// @ts-ignore
+import { gtag } from "ga-gtag";
 const ThemeSwitch = () => {
   const { theme, toggleTheme } = useTheme();
+  const handleClick = () => {
+    toggleTheme();
+    gtag("event", "theme_switch", {
+      event_category: "theme",
+      event_label: "theme_switch",
+    });
+  };
   return (
     <button
       className="fixed bottom-5 right-5 bg-white w-[3rem] h-[3rem] bg-opacity-80 backdrop-blur-[0.5rem] border border-white 
       border-opacity-40 shadow-2xl rounded-full flex items-center justify-center hover:scale-[1.15] active:scale-105 transition-all dark:bg-gray-950"
-      onClick={toggleTheme}
+      onClick={handleClick}
     >
       {theme === "light" ? <BsSun /> : <BsMoon />}
     </button>
