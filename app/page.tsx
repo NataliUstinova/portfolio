366710e374f5fb0bcbb4f0b4e57c765d2ebdac8a
Natali Ustinova <nata@purple.studio>
2023-12-08 13:41:21 +0200

    feat: experience section

--- app/page.tsx
+++ app/page.tsx
@@ -3,15 +3,17 @@ import SectionDivider from "@/components/section-divider";
 import About from "@/components/about";
 import Projects from "@/components/projects";
 import Skills from "@/components/skills";
+import Experience from "@/components/experience";
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center">
+    <main className="flex flex-col items-center px-4">
       <Intro />
       <SectionDivider />
       <About />
       <Projects />
       <Skills />
+      <Experience />
     </main>
   );
 }
